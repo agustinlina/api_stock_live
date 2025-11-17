3d48448ea2964b50b021e24482242ae6961d764b
agustin linares <agustinlinares2009@hotmail.com>
2025-11-17 11:27:57 -0300

    hola

--- api/stock_polo.js
+++ api/stock_polo.js
@@ -2,7 +2,7 @@ const { leerStockExcel } = require('./stock_utils');
 
 export default function handler(req, res) {
     try {
-        const productos = leerStockExcel('polo.XLS');
+        const productos = leerStockExcel('polo.xls');
         res.status(200).json(productos);
     } catch (error) {
         res.status(500).json({ error: 'No se pudo leer polo.xls' });
