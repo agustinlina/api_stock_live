4e2528a6b2259b699eb33d5e3e7ffbb4d820033d
Agustín Linares <agustinlinares2009@hotmail.com>
2025-11-17 22:42:11 -0300

    operapipi

--- api/stock_olav.js
+++ api/stock_olav.js
@@ -1,10 +1,24 @@
-const { leerStockExcel } = require('./stock_utils');
+const { leerStockExcel } = require('./stock_utils')
 
-export default function handler(req, res) {
+export default function handler (req, res) {
+  try {
+    let productos
+
     try {
-        const productos = leerStockExcel('olav.xls');
-        res.status(200).json(productos);
-    } catch (error) {
-        res.status(500).json({ error: 'No se pudo leer olav.xls' });
+      // Primero intenta con mayúsculas
+      productos = leerStockExcel('olav.XLS')
+    } catch (errorMayus) {
+      // Si falla, intenta con minúsculas
+      try {
+        productos = leerStockExcel('olav.xls')
+      } catch (errorMinus) {
+        // Si también falla, lanzo el error para que lo agarre el catch externo
+        throw errorMinus
+      }
     }
+
+    res.status(200).json(productos)
+  } catch (error) {
+    res.status(500).json({ error: 'No se pudo leer cba.XLS ni cba.xls.' })
+  }
 }
